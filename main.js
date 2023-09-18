e868c4da0737c056ec46fcc5b81ec9eac653c4d2
Stanciu Andrei <51262522+puya2003205@users.noreply.github.com>
2023-09-19 00:37:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Created by Bad_Bit
+
+
+
+var count = 0;
+
+function throwMe(card){
+    if(count%2==0){
+        card.classList.add("throwL");
+    }
+    else{
+        card.classList.add("throwR");
+    }
+    count ++ ;
+}
